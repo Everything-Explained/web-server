12ae67e5342ddae6071f4339eb322aa06030061b
Jaeiya <exanix@gmail.com>
2021-01-06 18:43:43 -0800

    Fix: `cacheControl: false` is not the same as `no-cache`

--- routes/route_api.ts
+++ routes/route_api.ts
@@ -8,6 +8,7 @@ const router = Router();
 router.get('/pageData/:file', (req, res, next) => {
   if (!req.params.file) return next();
   req.url = `/${req.params.file}`;
+  res.setHeader('Cache-Control', 'no-cache');
   staticGZIP('../web-client/release/web_client/_data',
     { serveStatic: { cacheControl: false } }
   )(req, res, next);
