3df25fabd9e562c322561fe2a304f40d0a7037a8
Jaeiya <exanix@gmail.com>
2021-01-15 13:13:47 -0800

    upd(API): data route should accept `dir` param
    `pageData` API method has been shortened to `data` and
    all assets are now in their own directory.

--- routes/api_route.ts
+++ routes/api_route.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 import staticGZIP from 'express-static-gzip';
 import { paths } from '../config';
 
+const debug = require('debug')('ee:api');
+
 
 const router = Router();
 
 
-router.get('/pageData/:file', (req, res, next) => {
+
+router.get('/data/:dir/:file', (req, res, next) => {
   if (!req.params.file) return next();
-  req.url = `/${req.params.file}`;
+  req.url = `${req.params.dir}/${req.params.file}`;
   res.setHeader('Cache-Control', 'public, no-cache');
   staticGZIP(`${paths.web}/_data`,
     { serveStatic: { cacheControl: false } }
