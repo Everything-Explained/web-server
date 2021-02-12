02698077b4d843f60410ef6bd22472452e39c074
Jaeiya <exanix@gmail.com>
2021-02-11 16:56:43 -0800

    Add temporary user database
    This is to facilitate passcode entry into the red33m
    page.

---
+++ database/users.ts
@@ -0,0 +1,43 @@
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { writeFile } from 'fs/promises';
+
+
+export type UserObj = { [key: string]: 'code'|'nocode' };
+
+if (!existsSync('./users.json')) {
+  writeFileSync('./users.json', JSON.stringify({}));
+}
+
+export const USERS: UserObj =
+  existsSync('./users.json')
+    ? JSON.parse(readFileSync('./users.json', 'utf-8'))
+    : {}
+;
+
+
+export function addUser(userid: string) {
+  USERS[userid] = 'nocode';
+  saveUsers();
+}
+
+
+export function updateUser(user: string, val: 'code'|'nocode') {
+  USERS[user] = val;
+  saveUsers();
+}
+
+
+export function getUserState(userid: string): "code" | "nocode" | undefined {
+  return USERS[userid] || undefined;
+}
+
+
+let fileQueue = -1
+;
+export function saveUsers() {
+  ++fileQueue;
+  setTimeout(async () => {
+    await writeFile('./users.json', JSON.stringify(USERS));
+    --fileQueue;
+  }, fileQueue * 250);
+}
