f6685ccb142b511b3fe317554d7e6148628a91f3
Jaeiya <exanix@gmail.com>
2021-05-03 19:10:54 -0700

    Use new QnA Form route handler

--- routes/api_route.ts
+++ routes/api_route.ts
@@ -4,30 +4,11 @@ import { paths } from '../config';
 import config from '../config.json';
 import argon from 'argon2';
 import { addUser, updateUser } from '../database/users';
-import mailer from 'nodemailer';
-import { inDev, yearInMs } from '../constants';
+import { yearInMs } from '../constants';
 import { readFile } from 'fs/promises';
+import { useQnaFormRoute } from './api/qnaForm';
 
-const debug = require('debug')('ee:api');
 
-const mailConfig = inDev
-? {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: config.auth.mailtrap.user,
-      pass: config.auth.mailtrap.pass,
-    }
-  }
-: {
-  service: 'Mailgun',
-  auth: {
-    user: config.auth.mailgun.user,
-    pass: config.auth.mailgun.pass
-  }
-};
-
-const _transport = mailer.createTransport(mailConfig);
 const _router = Router();
 const _staticData = staticGZIP(`${paths.web}/_data`, { serveStatic: { maxAge: yearInMs } });
 
@@ -91,29 +72,10 @@ _router.route('/auth/red33m')
     ;
     updateUser(req.id!, 'code');
     res.sendStatus(200);
-  })
-  // Send Exclusive Content Form for Red33m access
-  .post<any, any, AccessForm>((req, res) => {
-    const { name, email, questions } = req.body;
-
-    if (   !name                   || !name.trim().length  || name.length < 2
-        || !email                  || !email.trim().length || !email.includes('@')
-        || !questions              || !questions.length    || questions.length != 5
-        || !questions[0][1].length || !req.isAuthorized
-    ) return res.sendStatus(403);
-
-    _transport.sendMail({
-      from: `"${name}" <${email}>`,
-      to: 'ethankahn85@gmail.com',
-      subject: 'Everything Explained - EC Form',
-      html:
-`<div style="background-color: #0f1112; box-sizing: border-box; padding: 20px 5px 20px 15px; height: 100%; width: 100%; color: hsl(197, 11%, 70%); font-size: 1.1rem; font-family: Verdana;">
-${questions.reduce((pv, cv, i) => pv += `${i+1}.) ${cv[0]}<br><br><b style='color: hsl(161, 50%, 60%); font-weight: normal;'>${cv[1]}</b><br><br><br><br>`, '')}
-</div>`
-    })
-    .then(() => res.sendStatus(200))
-    .catch((err) => res.status(500).send(err.message));
   });
+
+
+useQnaFormRoute(_router);
 
 
 _router.get('*', (req, res) => {
